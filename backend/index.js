f8f1a94529a48a71358d10bc9bdf1e0e223742df
Bassem Wanies <bassemwanies2002@gmail.com>
2026-03-08 04:02:23 +0200

    Remove node_modules and add gitignore

--- backend/index.js
+++ backend/index.js
@@ -6,10 +6,10 @@ const fs =require('fs');
 
 app.use(express.json());
 
-app.use(express.static(path.join(__dirname, '../public')));
+app.use(express.static(path.join(__dirname, '../Vulnerable')));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname,'../public/home.html'));
+    res.sendFile(path.join(__dirname,'../Vulnerable/home.html'));
 });
 
 app.get('/api/paints',(req,res) => {
