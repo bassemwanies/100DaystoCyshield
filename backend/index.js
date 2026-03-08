15050a64455df5ea051df3a972a175c1e462008e
Bassem Wanies <bassemwanies2002@gmail.com>
2026-03-08 07:20:09 +0200

    updated paths

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 const path = require('path');
-const fs =require('fs');
+const fs = require('fs');
 const securePath = path.join(__dirname, '..', 'secure');
 
 app.use(express.json());
@@ -10,24 +10,24 @@ app.use(express.json());
 app.use(express.static(securePath));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(securePath,'home.html'));
+    res.sendFile(path.join(securePath, 'home.html'));
 });
 
-app.get('/api/paints',(req,res) => {
-    const data = fs.readFileSync('./paints.json', 'utf8');
+app.get('/api/paints', (req, res) => {
+    const data = fs.readFileSync(path.join(__dirname, '..', 'paints.json'), 'utf8');
     const paints = JSON.parse(data);
     res.json(paints);
 });
 
-app.get('/api/search',(req,res) => {
-    const query=req.query.query || "";
+app.get('/api/search', (req, res) => {
+    const query = req.query.query || "";
     const data = fs.readFileSync('paints.json', 'utf-8');
-    const paints=JSON.parse(data);
-    const results =paints.filter(paint =>
+    const paints = JSON.parse(data);
+    const results = paints.filter(paint =>
         paint.name.toLowerCase().includes(query.toLowerCase()) ||
         paint.painter.toLowerCase().includes(query.toLowerCase()) ||
         paint.description.toLowerCase().includes(query.toLowerCase())
-    
+
     );
     res.json(results);
 });
